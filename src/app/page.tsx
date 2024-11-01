06cfefe9c296739d7692a96a5ee13cc697ab7bc3
DoanNgocTrong <trong.doanngoc2023@gmail.com>
2024-11-01 14:26:59 +0700

    feat: logout hook

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,48 +1,29 @@
-// import { HeroForm } from '@/components/form';
-// import { Icons } from '@/components/icons';
-// import { Button } from '@/components/ui/button';
-// import * as m from '@/paraglide/messages';
+'use client';
+
+import { useEffect } from 'react';
+import { useRouter } from 'next/navigation';
 
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
-
-// const Home = () => {
-//   return (
-//     <section className="container mt-10 flex flex-col items-center gap-3 text-center md:absolute md:left-1/2 md:top-1/2 md:mt-0 md:-translate-x-1/2 md:-translate-y-1/2">
-//       <h1 className="mb-1 font-mono text-3xl font-extrabold leading-tight tracking-tighter md:text-4xl">
-//         {m.nextjs_starter_template_headline()}
-//       </h1>
-//       <p className="text-muted-foreground max-w-2xl">
-//         {m.nextjs_starter_template_description()}
-//       </p>
-//       <div className="mt-1">
-//         <HeroForm />
-//       </div>
-//       <div className="mt-2 flex gap-4">
-//         <Button asChild>
-//           <a
-//             href="https://github.com/Skolaczk/next-starter/blob/main/README.md#getting-started"
-//             target="_blank"
-//           >
-//             {m.get_started()}
-//           </a>
-//         </Button>
-//         <Button variant="outline" asChild>
-//           <a href="https://github.com/Skolaczk/next-starter" target="_blank">
-//             <Icons.github className="mr-2 size-4" /> {m.github()}
-//           </a>
-//         </Button>
-//       </div>
-//     </section>
-//   );
-// };
-
-// export default Home;
+import { useCurrent } from '@/features/auth/api/use-current';
+import { useLogout } from '@/features/auth/api/use-logout';
 
 export default function Home() {
+  const { data, isLoading } = useCurrent();
+
+  const { mutate } = useLogout();
+
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!data && !isLoading) {
+      router.push('/sign-in');
+    }
+  }, [data]);
+
   return (
     <div className="space-x-2 space-y-2 p-2">
-      <Button>primary button</Button>
+      <Button onClick={() => mutate()}>Log out</Button>
       <Button variant="destructive">destructive button</Button>
       <Button variant={'secondary'}>secondary button</Button>
       <Button variant={'ghost'}>ghost button</Button>
--- src/features/auth/server/route.ts
+++ src/features/auth/server/route.ts
@@ -7,8 +7,14 @@ import { AUTH_COOKIE } from '../contants';
 import { loginSchema, registerSchema } from '../schema';
 
 import { createAdminClient } from '@/lib/appwrite';
+import { sessionMiddleware } from '@/lib/session-middleware';
 
 const app = new Hono()
+  .get('/current', sessionMiddleware, async (c) => {
+    const user = c.get('user');
+
+    return c.json({ data: user });
+  })
   .post('/login', zValidator('json', loginSchema), async (c) => {
     const { email, password } = c.req.valid('json');
 
@@ -43,8 +49,12 @@ const app = new Hono()
 
     return c.json({ success: true });
   })
-  .post('/logout', (c) => {
+  .post('/logout', sessionMiddleware, async (c) => {
+    const account = c.get('account');
+
     deleteCookie(c, AUTH_COOKIE);
+    await account.deleteSession('current');
+
     return c.json({ success: true });
   });
 
