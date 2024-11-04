a360b88316f273e6393d05e299f676967f56571e
DoanNgocTrong <trong.doanngoc2023@gmail.com>
2024-11-04 11:41:26 +0700

    feat: implement workspace update functionality with image upload and member authorization

---
+++ src/features/workspaces/api/use-update-worksapce.ts
@@ -0,0 +1,49 @@
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { InferRequestType, InferResponseType } from 'hono';
+
+import { useToast } from '@/components/ui/use-toast';
+import { client } from '@/lib/rpc';
+
+type ResponseType = InferResponseType<
+  (typeof client.api.workspaces)[':workspaceId']['$patch'],
+  200
+>;
+type RequestType = InferRequestType<
+  (typeof client.api.workspaces)[':workspaceId']['$patch']
+>;
+
+export const useUpdateWorkspace = () => {
+  const queryClient = useQueryClient();
+
+  const { toast } = useToast();
+
+  const mutation = useMutation<ResponseType, Error, RequestType>({
+    mutationFn: async ({ form, param }) => {
+      const response = await client.api.workspaces[':workspaceId']['$patch']({
+        form,
+        param,
+      });
+
+      if (!response.ok) {
+        throw new Error('Failed to update workspace');
+      }
+
+      return await response.json();
+    },
+    onSuccess: ({ data }) => {
+      toast({
+        description: 'Workspace updated successfully',
+      });
+      queryClient.invalidateQueries({ queryKey: ['workspaces'] });
+      queryClient.invalidateQueries({ queryKey: ['workspaces', data.$id] });
+    },
+    onError: () => {
+      toast({
+        description: 'Faild to create workspace',
+        variant: 'destructive',
+      });
+    },
+  });
+
+  return mutation;
+};
--- src/features/workspaces/server/route.ts
+++ src/features/workspaces/server/route.ts
@@ -2,9 +2,10 @@ import { zValidator } from '@hono/zod-validator';
 import { Hono } from 'hono';
 import { ID, Query } from 'node-appwrite';
 
-import { createWorkspaceSchema } from '../schemas';
+import { createWorkspaceSchema, updateWorkspaceSchema } from '../schemas';
 
 import { MemberRole } from '@/features/members/type';
+import { getMember } from '@/features/members/utils';
 import {
   DATABASE_ID,
   IMAGES_BUCKET_ID,
@@ -88,6 +89,60 @@ const app = new Hono()
         workspaceId: workspace.$id,
         role: MemberRole.ADMIN,
       });
+
+      return c.json({ data: workspace });
+    }
+  )
+  .patch(
+    '/:workspaceId',
+    sessionMiddleware,
+    zValidator('form', updateWorkspaceSchema),
+    async (c) => {
+      const databases = c.get('databases');
+      const storage = c.get('storage');
+      const user = c.get('user');
+
+      const { workspaceId } = c.req.param();
+      const { name, image } = c.req.valid('form');
+
+      const member = await getMember({
+        databases,
+        workspaceId,
+        userId: user.$id,
+      });
+
+      if (!member || member.role !== MemberRole.ADMIN) {
+        return c.json({ error: 'Unauthorized' }, 401);
+      }
+
+      let uploadedImageUrl: string | undefined;
+
+      if (image instanceof File) {
+        const file = await storage.createFile(
+          IMAGES_BUCKET_ID,
+          ID.unique(),
+          image
+        );
+
+        const arrayBuffer = await storage.getFilePreview(
+          IMAGES_BUCKET_ID,
+          file.$id
+        );
+
+        uploadedImageUrl = `data:image/png;base64,${Buffer.from(arrayBuffer).toString('base64')}`;
+      } else {
+        uploadedImageUrl = image;
+      }
+
+      const workspace = await databases.updateDocument(
+        DATABASE_ID,
+        WORKSPACES_ID,
+        workspaceId,
+        {
+          name,
+          imageUrl: uploadedImageUrl,
+        }
+      );
 
       return c.json({ data: workspace });
     }
