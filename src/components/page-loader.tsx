b8c2bfdca62ff4b023ab0641356f759d01071951
DoanNgocTrong <trong.doanngoc2023@gmail.com>
2024-11-11 11:17:51 +0700

    refactor: update PageLoader component styles

--- src/components/page-loader.tsx
+++ src/components/page-loader.tsx
@@ -2,7 +2,7 @@ import { Loader } from 'lucide-react';
 
 export const PageLoader = () => {
   return (
-    <div className="flex h-full flex-col items-center justify-center">
+    <div className="flex h-screen flex-col items-center justify-center">
       <Loader className="text-muted-foreground size-6 animate-spin" />
     </div>
   );
